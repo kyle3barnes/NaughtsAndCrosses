77e50eaa9921428f52fbf283c7e3f24515abd46b
Kyle Barnes <kylebarnes2003@live.co.uk>
2021-12-15 13:01:20 +0000

    fixes and computer choices

--- main.py
+++ main.py
@@ -7,12 +7,26 @@ def setup_board():
     board = ""
 
     for space in spaces:
-        if space == 9:
+        counter = 1  # PROBLEM WITH COUNTER - WHEN COUNTER STARTS AT 1, SPACE ISN'T RECOGNISED AS BEING EQUAL TO 0 WHEN SPACE % 3
+        #                                     WHEN COUNTER STARTS AT 0, THE FIRST SPACE IS DISPLAYED AS IT IS WHEN COUNTER % 3 = 0
+
+        if space == 'X' or space == 'O':
+            if counter == 9:
+                board += space
+            elif counter % 3 == 0:
+                board += space + "\n- - - - - \n"
+            else:
+                board += space + " | "
+        elif space == 9:
             board += str(space)
         elif space % 3 == 0:
             board += (str(space) + "\n- - - - - \n")
         else:
             board += (str(space) + " | ")
+        counter += 1
+
+    print(board)
+
     return spaces, board
 
 
@@ -32,32 +46,34 @@ def get_user_choice():
 def get_shape():
     while True:
         try:
-            user_input = input("Enter the shape you would like to play ['O' or 'X']: ")
-            while user_input not in ['O', 'X']:
-                user_input = input("Error, enter 'O' for naughts and 'X' for crosses: ")
+            shapes = ['O', 'X']
+            user_input = (input("Enter the shape you would like to play ['O' or 'X']: ")).upper()
+            while user_input not in shapes:
+                user_input = (input("Error, enter 'O' for naughts and 'X' for crosses: ")).upper()
         except ValueError:
             print("Error, enter 'O' for naughts and 'X' for crosses: ")
             continue
         else:
-            return user_input
+            comp_shape = shapes[(len(shapes) - 1) - shapes.index(user_input)]
+            return user_input, comp_shape
 
 
 def get_comp_choice_random():
-    comp_choice = random.randint(1, 10)
+    comp_choice = random.randint(1, 9)
     return comp_choice
 
 
-def update_board(user_choice, shape):
-    spaces[user_choice] = shape
+def update_board(choice, shape):
+    spaces[choice - 1] = shape
     setup_board()
 
 
 if __name__ == '__main__':
     spaces, board = setup_board()
-    print(board)
     print()
     print("callum gay")
+    user_shape, comp_shape = get_shape()
     user_choice = get_user_choice()
-    shape = get_shape()
-    update_board(user_choice, shape)
+    update_board(user_choice, user_shape)
     comp_choice = get_comp_choice_random()
+    update_board(comp_choice, comp_shape)
